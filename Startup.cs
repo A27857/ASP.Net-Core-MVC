35086af051f73e6125d823f3fe893b2b52323825
Dam Tuan Dat <dat.dam.intern@ntq-solution.com.vn>
2021-06-25 11:25:11 +0700

    Ex5

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +27,6 @@ namespace ASP_.Net_Core_Fundamental
         {
             services.AddControllersWithViews();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -40,21 +40,24 @@ namespace ASP_.Net_Core_Fundamental
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection(); 
-            app.UseStaticFiles(); 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
-            
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
+                name: "myroute",
+                pattern: "NashTech/{controller=Home}/{action=Index}/{id?}");
             });
-             //app.UseMvc(routes =>
+            //app.UseMvc(routes =>
             //{
             //    routes.MapRoute("default",
             //                    "{controller=Home}/{action=Index}/{id?}");
